7a263d2ad6442fa52c4f56d645a8593edd1b2815
Xavier Au <xavier.au@anacreation.com>
2016-06-02 16:23:15 +0800

    config post webhook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,19 +11,19 @@
 |
 */
 
+
+
 use Illuminate\Http\Request;
 
 const VERIFY_TOKEN = "B45ycLXmHCqDNuPwybTZCYoInhHEiAJbuGkZC2kwVv4eC41ISgYcNmkKpZC5W9Tl3sASpQZDZD";
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::post("/webhook", function(Request $request){
-    if($request->get("hub.verify_token") === VERIFY_TOKEN){
-        return $request->get("hub.challenge");
-    }
-    return "Error, wrong validation token";
+    return $request->all();
 });
 
 Route::get("/webhook", function(Request $request){
