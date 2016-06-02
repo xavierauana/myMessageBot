98eebce97370b45b217404478ef5b4451293ad26
Xavier Au <xavier.au@anacreation.com>
2016-06-02 18:41:02 +0800

    config post webhook 18

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,6 +21,11 @@ const PAGE_TOKEN = "EAAWKZCVRmZBbwBAHgw5elHvQj3jzHrYpKqkrcfLi1FfZCpHN21T15gQZCEZ
 const MESSAGE_URL = "https://graph.facebook.com/v2.6/me/messages";
 
 Route::get('/', function () {
+//    $data = [
+//        "recipient" => ["id" => '121212'],
+//        "message"   => "something"
+//    ];
+//    dd(json_encode($data));
 
     dd(unserialize('a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856644718;s:9:"messaging";a:1:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856166415;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856166394:6e359cd0890102d746";s:3:"seq";i:6;s:4:"text";s:7:"another";}}}}}}  
 [2016-06-02 08:37:25] local.INFO: a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856645371;s:9:"messaging";a:2:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856330413;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856330405:8fd459117e0126cc73";s:3:"seq";i:7;s:4:"text";s:3:"try";}}i:1;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856620897;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856620889:261ec172250e728f67";s:3:"seq";i:8;s:4:"text";s:12:"anothere try";}}}}}}  
@@ -47,21 +52,22 @@ Route::post("/webhook", function (Request $request) {
         "message"   => $messageData
     ];
 
+    $options =  [
+        'content-type'=>"application/json; charset=utf8",
+        'query'=>['access_token'=>PAGE_TOKEN]
+    ];
+    $body = json_encode($data);
+
     $client = new \GuzzleHttp\Client();
 
-    $request = new \GuzzleHttp\Psr7\Request("POST", $uri, [
-        'content-type'=>"application/json; charset=utf8",
-        'query'=>['access_token'=>PAGE_TOKEN],
-        'body' => json_encode($data)
-    ]);
-    
-    Log::info($request);
+    $request = new \GuzzleHttp\Psr7\Request("POST", $uri, $options, $body);
 
     $promise = $client->sendAsync($request)->then(
         function (ResponseInterface $response) {
             Log::info(serialize($response->getStatusCode()));
         },
         function (RequestException $response) {
+            Log::info(serialize($response->getRequest()));
             Log::info(serialize($response->getMessage()));
         });
 
