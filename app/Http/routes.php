675bb5017a45204db2f365e9c4a891b5a3e869a8
Xavier Au <xavier.au@anacreation.com>
2016-06-02 18:17:51 +0800

    config post webhook 12

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,10 @@
 |
 */
 
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
 
 const VERIFY_TOKEN = "B45ycLXmHCqDNuPwybTZCYoInhHEiAJbuGkZC2kwVv4eC41ISgYcNmkKpZC5W9Tl3sASpQZDZD";
 const PAGE_TOKEN = "EAAWKZCVRmZBbwBAHgw5elHvQj3jzHrYpKqkrcfLi1FfZCpHN21T15gQZCEZBmiaGcLzIzuzbHJ0ZAJ89GSSLq49yWPaadEk4ZAXnbZAV8JOEo811fwkKrlRe4Dd4Sp1aNt80SP3Q907HmYbkf7wgm9MZC9PFInrHqn1DaWJxXGY5EDAZDZD";
@@ -23,47 +25,51 @@ Route::get('/', function () {
     dd(unserialize('a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856644718;s:9:"messaging";a:1:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856166415;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856166394:6e359cd0890102d746";s:3:"seq";i:6;s:4:"text";s:7:"another";}}}}}}  
 [2016-06-02 08:37:25] local.INFO: a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856645371;s:9:"messaging";a:2:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856330413;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856330405:8fd459117e0126cc73";s:3:"seq";i:7;s:4:"text";s:3:"try";}}i:1;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856620897;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856620889:261ec172250e728f67";s:3:"seq";i:8;s:4:"text";s:12:"anothere try";}}}}}}  
 '));
+
     return view('welcome');
 });
 
-Route::post("/webhook", function(Request $request){
+Route::post("/webhook", function (Request $request) {
 
-    $query = http_build_query(['access_token'=>PAGE_TOKEN]);
-    $uri = MESSAGE_URL."?$query";
+    $query = http_build_query(['access_token' => PAGE_TOKEN]);
+    $uri = MESSAGE_URL . "?$query";
 
     $incomingMessage = new \App\Entity\IncomingMessage($request);
 
-    Log::info(serialize( $incomingMessage ));
+    Log::info(serialize($incomingMessage));
     Log::info($uri);
 
     $messageData = $incomingMessage->message;
     $senderId = $incomingMessage->senderId;
 
     $data = [
-        "recipient"=> [ "id"=>$senderId ],
-        "message"=> $messageData
+        "recipient" => ["id" => $senderId],
+        "message"   => $messageData
     ];
 
     $client = new \GuzzleHttp\Client();
 
-    $request = new \GuzzleHttp\Psr7\Request("POST", $uri,[
-        'json'=>json_encode($data)
+    $request = new \GuzzleHttp\Psr7\Request("POST", $uri, [
+        'json' => json_encode($data)
     ]);
 
-    $promise = $client->sendAsync($request)->then(function($response){
-        Log::info(serialize($response));
-    }, function($response){
-        Log::info(serialize($response));
-    });
+    $promise = $client->sendAsync($request)->then(
+        function (ResponseInterface $response) {
+            Log::info(serialize($response->getStatusCode()));
+        },
+        function (RequestException $response) {
+            Log::info(serialize($response->getMessage()));
+        });
 
     $promise->wait();
 
     return response(null, 200);
 });
 
-Route::get("/webhook", function(Request $request){
-    if($request->get("hub_verify_token") === VERIFY_TOKEN){
+Route::get("/webhook", function (Request $request) {
+    if ($request->get("hub_verify_token") === VERIFY_TOKEN) {
         return $request->get("hub_challenge");
     }
+
     return "Error, wrong validation token";
 });
