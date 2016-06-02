2175c87c13538b2471d8db335a07e20a479204f0
Xavier Au <xavier.au@anacreation.com>
2016-06-02 17:50:30 +0800

    config post webhook 5

---
+++ app/Entity/IncomingMessage.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Author: Xavier Au
+ * Date: 2/6/2016
+ * Time: 5:21 PM
+ */
+
+namespace App\Entity;
+
+
+
+use Illuminate\Http\Request;
+
+class IncomingMessage
+{
+    public $object;
+    public $entryId;
+    public $entryTime;
+    public $senderId;
+    public $recipientId;
+    public $objectTime;
+    public $messageTime;
+    public $messageId;
+    public $messageSeq;
+    public $message;
+
+
+    public function __construct(Request $request) {
+        $this->entryId = $this->getEntryId($request);
+        $this->entryTime = $this->getEntryTime($request);
+        $this->message = $this->getMessage($request);
+        $this->messageId = $this->getMessageId($request);
+        $this->messageSeq = $this->getMessageSeq($request);
+        $this->messageTime = $this->getMessageTime($request);
+        $this->object = $this->getObject($request);
+        $this->recipientId = $this->getRecipientId($request);
+        $this->senderId = $this->getSenderId($request);
+    }
+
+    private function getEntryId($request)
+    {
+        return $request->get('entry')[0]['id'];
+    }
+
+    private function getEntryTime($request)
+    {
+        return $request->get('entry')[0]['time'];
+    }
+
+    private function getMessage($request)
+    {
+        return $request->get('entry')[0]['messaging']['message']['text'];
+    }
+
+    private function getMessageId($request)
+    {
+        return $request->get('entry')[0]['messaging']['message']['mid'];
+    }
+
+    private function getMessageSeq($request)
+    {
+        return $request->get('entry')[0]['messaging']['message']['seq'];
+    }
+
+    private function getMessageTime($request)
+    {
+        return $request->get('entry')[0]['messaging']['timestamp'];
+    }
+
+    private function getObject($request)
+    {
+        return $request->get('object');
+    }
+
+    private function getRecipientId($request)
+    {
+        return $request->get('entry')[0]['messaging']['recipient']['id'];
+    }
+
+    private function getSenderId($request)
+    {
+        return $request->get('entry')[0]['messaging']['sender']['id'];
+    }
+
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,13 +15,34 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 const VERIFY_TOKEN = "B45ycLXmHCqDNuPwybTZCYoInhHEiAJbuGkZC2kwVv4eC41ISgYcNmkKpZC5W9Tl3sASpQZDZD";
+const PAGE_TOKEN = "EAAWKZCVRmZBbwBAHgw5elHvQj3jzHrYpKqkrcfLi1FfZCpHN21T15gQZCEZBmiaGcLzIzuzbHJ0ZAJ89GSSLq49yWPaadEk4ZAXnbZAV8JOEo811fwkKrlRe4Dd4Sp1aNt80SP3Q907HmYbkf7wgm9MZC9PFInrHqn1DaWJxXGY5EDAZDZD";
+const MESSAGE_URL = "https://graph.facebook.com/v2.6/me/messages";
 
 Route::get('/', function () {
+
+    dd(unserialize('a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856644718;s:9:"messaging";a:1:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856166415;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856166394:6e359cd0890102d746";s:3:"seq";i:6;s:4:"text";s:7:"another";}}}}}}  
+[2016-06-02 08:37:25] local.INFO: a:2:{s:6:"object";s:4:"page";s:5:"entry";a:1:{i:0;a:3:{s:2:"id";s:16:"1768147536755704";s:4:"time";i:1464856645371;s:9:"messaging";a:2:{i:0;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856330413;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856330405:8fd459117e0126cc73";s:3:"seq";i:7;s:4:"text";s:3:"try";}}i:1;a:4:{s:6:"sender";a:1:{s:2:"id";s:17:"10154043934195239";}s:9:"recipient";a:1:{s:2:"id";s:16:"1768147536755704";}s:9:"timestamp";i:1464856620897;s:7:"message";a:3:{s:3:"mid";s:36:"mid.1464856620889:261ec172250e728f67";s:3:"seq";i:8;s:4:"text";s:12:"anothere try";}}}}}}  
+'));
     return view('welcome');
 });
 
 Route::post("/webhook", function(Request $request){
-    Log::info(serialize($request->all()));
+    $incomingMessage = new \App\Entity\IncomingMessage($request);
+    Log::info(serialize( $incomingMessage ));
+    $messageData = "hello";
+    $senderId = $incomingMessage->senderId;
+    $data = [
+        "recipient"=> ["id"=>$senderId],
+        "message"=> $messageData
+    ];
+    $query = http_build_query(['access_token'=>PAGE_TOKEN]);
+    $client = new \GuzzleHttp\Client();
+    $request = new \GuzzleHttp\Psr7\Request("POST", MESSAGE_URL."?$query",[
+        'json'=>$data
+    ]);
+    $client->sendAsync($request)->then(function($response){
+        Log::info(serialize($response));
+    });
     return response(null, 200);
 });
 
